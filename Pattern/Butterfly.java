367702e08aea67a0f62904a8f2dda57d7b7cdeb6
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-03 19:54:36 +0530

    Solid_rhombus is done

---
+++ Pattern/Butterfly.java
@@ -0,0 +1,8 @@
+package Pattern;
+import java.util.*;
+public class Butterfly {
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- Pattern/Solid_Rhombus.java
+++ Pattern/Solid_Rhombus.java
@@ -1,7 +1,22 @@
 package Pattern;
-
+import java.util.*;
 public class Solid_Rhombus {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int space =n-1;
         
+        for(int line =1; line<=n; line++){
+            //space
+            for(int j =1;j<=space;j++){
+                System.out.print("  ");
+            }
+            //star
+            for(int j=1;j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            space--;
+        }
     }
 }
