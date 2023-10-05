a1035db8208a4845e3073f06072dcdfc27927413
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-05 17:00:49 +0530

    factorial of n

--- Fun_Method/Factorial.java
+++ Fun_Method/Factorial.java
@@ -1,5 +1,19 @@
 package Fun_Method;
 
 public class Factorial {
-    
+    public static int factorial(int num) {
+        int fact = 1;
+        if (num == 0) {
+            return fact;
+        } else {
+            for (int i = 1; i <= num; i++) {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(0));
+    }
 }
