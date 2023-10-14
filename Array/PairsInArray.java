45eb0875bdce28b176ad49b8d163cc7095e77e18
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-14 14:48:56 +0530

    Completed TrappedWater  with O(n^2) complexity

--- Array/PairsInArray.java
+++ Array/PairsInArray.java
@@ -4,7 +4,7 @@ public class PairsInArray {
     public static void PairinArray(int arr[]) {
         int tp=0;
         for (int i = 0; i <= arr.length-2; i++) {
-            int curr = i;
+            
             
             for(int j=i+1; j<arr.length; j++){
                 System.out.print("("+arr[i]+","+arr[j]+")");
--- Array/TrappingRainWater.java
+++ Array/TrappingRainWater.java
@@ -1,51 +1,59 @@
 package Array;
 
 public class TrappingRainWater {
-    public static void LeftMax(int height[]) {
-        int LeftArray[] = new int[height.length];
+    public static void LeftMax(int height[], int LeftArray[]) {
+
         int leftmax = Integer.MIN_VALUE;
         LeftArray[0] = height[0];
         for (int i = 1; i < height.length; i++) {
-
             for (int j = i; j >= 0; j--) {
-
                 if (leftmax < height[j]) {
                     leftmax = height[j];
                 }
             }
             LeftArray[i] = leftmax;
             leftmax = 0;
-
         }
-        System.out.println(LeftArray[0]);
-        for (int i = 0; i < LeftArray.length; i++) {
-            System.out.print(LeftArray[i]+ "  ");
-        }
+       
 
     }
-    public static void RightMax(int height[]) {
-        int RightArray[] = new int[height.length];
+
+    public static void RightMax(int height[], int RightArray[]) {
+
         int rightmax = Integer.MIN_VALUE;
-        RightArray[RightArray.length-1]= height[height.length-1];
-        for(int i = (height.length)-1;i>=0 ;i--){
-            for(int j =i ; j<(height.length);j++){
-                if(rightmax<height[j]){
-                    rightmax= height[j];
+        RightArray[RightArray.length - 1] = height[height.length - 1];
+        for (int i = (height.length) - 1; i >= 0; i--) {
+
+            for (int j = i; j < (height.length); j++) {
+
+                if (rightmax < height[j]) {
+                    rightmax = height[j];
                 }
             }
             RightArray[i] = rightmax;
-            rightmax = 0; 
+            rightmax = 0;
         }
-        for(int i=0;i<RightArray.length;i++){
-            System.out.print(RightArray[i] + "  ");
-        }
+        
     }
+
 
     public static void main(String[] args) {
         int height[] = { 4, 2, 0, 6, 3, 2, 5 };
-        LeftMax(height);
-        System.out.println();
-        RightMax(height);
+        int LeftArray[] = new int[height.length];
+        int RightArray[] = new int[height.length];
+        int width = 1;
+        int TotalTrappedWater= 0;
+        LeftMax(height, LeftArray);
+        RightMax(height, RightArray);
+
+
+        for(int i =1; i<height.length; i++){
+          int waterlevel = Math.min(LeftArray[i], RightArray[i]);
+          int Trappedwater = ((waterlevel) - height[i])*width;
+          
+          TotalTrappedWater += Trappedwater;
+        }
+       System.out.println(TotalTrappedWater);
         
 
     }
