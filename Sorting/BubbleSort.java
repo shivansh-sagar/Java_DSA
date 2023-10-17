ec978d25229e9cad2e4a977a927f0b42a7d46b83
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-17 15:53:52 +0530

    Selection Sort is done

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -19,8 +19,6 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
         bubbleSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+       System.out.println(arr);
     }
 }
