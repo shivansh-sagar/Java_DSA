d28c5c4e7f9e47bb7a3ad05436be91301f0bbcae
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-21 22:29:29 +0530

    Counting Sort is done

---
+++ Sorting/Assignment1.java
@@ -0,0 +1,12 @@
+package Sorting;
+
+public class Assignment1 {
+    public static void bubble(int arr[]){
+        for(int i =0 ; i<arr.length; i++){
+            int curr = i;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]= {3,6,2,1,8,7,4,5,3,1};
+    }
+}
