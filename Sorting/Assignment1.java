62152b6401b6cd7bc1833aaa1711920d6d245fdb
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-21 22:46:00 +0530

    Assignment 1 is done

--- Sorting/Assignment1.java
+++ Sorting/Assignment1.java
@@ -1,12 +1,28 @@
 package Sorting;
 
 public class Assignment1 {
-    public static void bubble(int arr[]){
-        for(int i =0 ; i<arr.length; i++){
-            int curr = i;
+    public static void bubble(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
     }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[]= {3,6,2,1,8,7,4,5,3,1};
+        int arr[] = { 3,6,2,1,8,7,4,5,3,1 };
+        
+        bubble(arr);
+        printarr(arr);
     }
 }
--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -15,10 +15,15 @@ public class BubbleSort {
             }
         }
     }
+    public static void printarr(int arr[]){
+        for(int i =0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
 
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
         bubbleSort(arr);
-       System.out.println(arr);
+        printarr(arr);
     }
 }
