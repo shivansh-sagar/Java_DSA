04b7571757fc08064cd3e6b4ac77d98c26391a5e
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-25 20:47:53 +0530

    Practice que Largest and samllest

---
+++ TwoDArray/LargestAndSmallest.java
@@ -0,0 +1,31 @@
+package TwoDArray;
+import java.util.Scanner;
+public class LargestAndSmallest {
+    public static void LargeAndSmall(int matrix[][]){
+        int Largest= Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0; j<matrix[0].length;j++){
+                if(matrix[i][j]>Largest){
+                    Largest=matrix[i][j];
+                }
+                if(matrix[i][j]<smallest){
+                    smallest = matrix[i][j];
+                }
+            }
+        }
+        System.out.println(Largest);
+        System.out.println(smallest);
+
+    }
+    public static void main(String[] args) {
+        int matrix[][]= new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0; j<matrix[0].length;j++){
+                matrix[i][j]= sc.nextInt();
+            }
+        }
+        LargeAndSmall(matrix);
+    }
+}
