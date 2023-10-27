4c59170833711d1d1755a4aa57a6e45b8ded1c35
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-27 11:55:55 +0530

    Classic spiral problem

--- TwoDArray/SpiralMatrix.java
+++ TwoDArray/SpiralMatrix.java
@@ -1,23 +1,53 @@
 package TwoDArray;
 
+
+
 public class SpiralMatrix {
     public static void printSpiral(int matrix[][]) {
-        int SR = 0;
-        int ER = matrix.length - 1;
-        int SC = 0;
-        int EC = matrix[0].length - 1;
-        while (SR > ER / 2 && SC > EC / 2) {
+        int StartRow = 0;
+        int EndRow = matrix.length - 1;
+        int StartCol = 0;
+        int EndCol = matrix[0].length - 1;
+        while (StartRow <= EndRow && StartCol <= EndCol) {
             // TOP
+            for (int i = StartCol; i <= EndCol; i++) {
+                System.out.print(matrix[StartRow][i] + " ");
+            }
+
             // RIGHT
+            for (int j = StartRow + 1; j <= EndRow; j++) {
+                System.out.print(matrix[j][EndCol] + " ");
+            }
+
             // BOTTOM
+            for (int i = EndCol - 1; i >= StartCol; i--) {
+                if(StartRow==EndRow){
+                    break;
+                }
+                System.out.print(matrix[EndRow][i] + " ");
+            }
+
             // LEFT
+            for (int j = EndRow - 1; j >= StartRow + 1; j--) {
+                if(StartCol==EndCol){
+                    break;
+                }
+                System.out.print(matrix[j][StartCol] + " ");
+            }
+
+            StartRow++;
+            EndRow--;
+            StartCol++;
+            EndCol--;
         }
     }
 
     public static void main(String[] args) {
         int matrix[][] = { { 1, 2, 3, 4 },
                 { 5, 6, 7, 8 },
-                { 9, 10, 11, 12 },
-                { 13, 14, 15, 16 } };
+                { 9, 10, 11, 12 }
+                 };
+
+        printSpiral(matrix);
     }
 }
