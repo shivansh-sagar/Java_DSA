10ac7705ca6e17d0e83c9b0bb286ebec9883ca70
shivansh-sagar <shivanshsagar987@gmail.com>
2023-10-31 21:17:30 +0530

    And OR  One's complement

---
+++ Bit_Manipulation/Introduction.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+public class Introduction {
+    public static void main(String[] args){
+        // Bit Manipulation
+        int a =5, b=6;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(~0);
+    }
+}
