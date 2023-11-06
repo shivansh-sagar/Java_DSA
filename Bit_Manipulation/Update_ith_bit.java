50229885b3d9ae6188938f7d5c80f06c642cd6c9
shivansh-sagar <shivanshsagar987@gmail.com>
2023-11-06 20:48:01 +0530

    Update ith bit

--- Bit_Manipulation/Update_ith_bit.java
+++ Bit_Manipulation/Update_ith_bit.java
@@ -11,18 +11,24 @@ public class Update_ith_bit {
         return n & BitMask;
     }
 
-    public static int UpdateIthBit(int n, int i, int userBit){
+    public static int UpdateIthBit1(int n, int i, int userBit){
         if(userBit==0){
             return Clear_IthBit(n,i);
         }else{
             return Set_IthBit(n, i);
         }
     }
+    public static int UpdateIthBit2(int n, int i, int userBit){
+        n=Clear_IthBit(n, i);
+        int BitMask=userBit<<i;
+        return n|BitMask;
+    }
 
     public static void main(String[] args) {
         int n = 10;
         int i = 2;
-        int userBit = 1;
-        System.out.println(UpdateIthBit(n, i, userBit));
+        int userBit = 0;
+        System.out.println(UpdateIthBit1(n, i, userBit));
+        System.out.println(UpdateIthBit2(n, i, userBit));
     }
 }
