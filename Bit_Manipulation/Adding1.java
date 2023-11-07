ea4c5eee3837c1e939ed2b9db59135a21b55f4e2
shivansh-sagar <shivanshsagar987@gmail.com>
2023-11-07 13:51:13 +0530

    Adding 1 to the integer usin Bitwise ~NOT

---
+++ Bit_Manipulation/Adding1.java
@@ -0,0 +1,10 @@
+package Bit_Manipulation;
+
+public class Adding1 {
+    public static void main(String[] args) {
+        int a=5, x=8;
+        System.out.println(-~a);
+        System.out.println(x + " + " + 1 + " is " + -~x);
+
+    }
+}
