57009118224b077827ff5e93102569204ab67664
shivansh-sagar <shivanshsagar987@gmail.com>
2023-11-09 13:02:03 +0530

    Understanding the concept of private -- public

--- OOPS/AccessModifiers.java
+++ OOPS/AccessModifiers.java
@@ -1,5 +1,20 @@
 package OOPS;
 
 public class AccessModifiers {
+    public static void main(String[] args) {
+        BankAccount myAcc = new BankAccount();
+        myAcc.UserName = "Shivanshu";
+        myAcc.setPassword("abcd");
+        System.out.println(myAcc.UserName);
+        
+        
+    }
     
 }
+class BankAccount{
+        public String UserName;
+        private String Password = "abcd1234";
+        public void setPassword(String pwd){
+            Password = pwd;
+        }
+    }
