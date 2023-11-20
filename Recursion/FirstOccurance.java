f21dfc1b1d95435a06d952e7cd5c4078bd5cb7ea
shivansh-sagar <shivanshsagar987@gmail.com>
2023-11-20 21:41:30 +0530

    First occurance of a number using Recursion

---
+++ Recursion/FirstOccurance.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class FirstOccurance {
+    public static int firstOcc(int arr[],int key, int i){
+        if(arr[i]==key){
+            return i;
+        }
+        if(i>arr.length-1){
+            return -1;
+        }
+        return firstOcc(arr, key, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={8,3,6,9,51,10,2,5};
+        int key = 5;
+        System.out.println(firstOcc(arr, key, 0));
+    }
+}
