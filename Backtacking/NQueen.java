4b37d2c31e7263b8980edeb981a9582978fbdd32
shivansh-sagar <shivanshsagar987@gmail.com>
2023-12-15 21:59:15 +0530

    Counting number of posible solution

--- Backtacking/NQueen.java
+++ Backtacking/NQueen.java
@@ -1,22 +1,22 @@
 package Backtacking;
 
 public class NQueen {
-    public static boolean isSafe(char board[][], int row, int col){
-        //vertical up
-        for(int i = row-1; i>=0; i--){
-            if (board[i][col]=='Q') {
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
                 return false;
             }
         }
-        //diagonal left up
-        for(int i =row-1, j=col-1 ;i>=0 && j>=0;i--, j--){
-                if(board[i][j]=='Q'){
-                    return false;
-                }
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
         }
-        //diagonal right up
-        for(int i= row-1, j=col+1; i>=0 && j<board.length; i--, j++){
-            if(board[i][j]=='Q'){
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
                 return false;
             }
         }
@@ -26,8 +26,9 @@ public class NQueen {
     public static void nQueen(char board[][], int row) {
         // base
         if (row == board.length) {
-            printBoard(board);
-            return;
+            // printBoard(board);
+            count++;
+            return ;
         }
         // col loop
         for (int j = 0; j < board.length; j++) {
@@ -37,7 +38,6 @@ public class NQueen {
                 board[row][j] = 'x'; // Backtracking step
             }
 
-            
         }
     }
 
@@ -51,16 +51,18 @@ public class NQueen {
         }
 
     }
-
+    static int count = 0;
     public static void main(String[] args) {
         int n = 5;
         char board[][] = new char[n][n];
-        //initialize
+        // initialize
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 board[i][j] = 'x';
             }
         }
         nQueen(board, 0);
+        System.out.println(count);
     }
+    
 }
