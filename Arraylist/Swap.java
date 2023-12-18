3a49c28f5714c64534e103c8145fe953268f4493
shivansh-sagar <shivanshsagar987@gmail.com>
2023-12-19 01:00:04 +0530

    Swaping the two number in ArrayList

---
+++ Arraylist/Swap.java
@@ -0,0 +1,27 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class Swap {
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+
+        System.out.println(list);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        
+
+        int idx=1, idx2=3;
+        swap(list, idx, idx2);
+
+    
+    }
+}
