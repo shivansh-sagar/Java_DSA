195a74d3f4c7ca3bc49bdfd2fe90a5be8f9a375b
shivansh-sagar <shivanshsagar987@gmail.com>
2023-12-19 00:37:04 +0530

    Reverse the number using array list

--- Arraylist/ReverseArrayList.java
+++ Arraylist/ReverseArrayList.java
@@ -1,5 +1,17 @@
 package Arraylist;
 
+import java.util.ArrayList;
+
 public class ReverseArrayList {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i < 6; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+
+        for (int i = list.size()-1; i >= 0; i--) {
+            System.out.print(list.get(i)+ " ");
+        }
+    }
 }
