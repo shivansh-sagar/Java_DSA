3aea9a3de34912e0b9eb82ed615bda489cf3774f
shivansh-sagar <shivanshsagar987@gmail.com>
2024-02-26 14:27:08 +0530

    pairsumtwo is solved

---
+++ Arraylist/PairSumTwo.java
@@ -0,0 +1,66 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class PairSumTwo {
+    public static boolean PairSumFun(ArrayList<Integer> list, int target){
+        int lp=0;
+        int rp=0;
+        int i=0;
+        while (list.get(i) < list.get(i+1)) {
+            lp= i+2;
+            rp= i+1;
+            i++;
+        }
+        while(lp!=rp){
+            //case1
+            if(list.get(lp)+list.get(rp)== target){
+                return true ;
+            }
+            //case2
+            if(lp!=list.size()-1){
+                if(list.get(lp)+list.get(rp)>target){
+                    rp--;
+                }
+            }else{
+                lp=0;
+            }
+            
+            //case3
+            if(rp!=0){
+                if(list.get(lp)+list.get(rp)<target){
+                lp++;
+                }
+            }else{
+                rp=list.size()-1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        int target= 30;
+        list.add(0, 11);
+        list.add(1, 15);
+        list.add(2, 6);
+        list.add(3, 8);
+        list.add(4, 9);
+        list.add(5, 10);
+        System.out.println(list);
+        int lp=0;
+        int rp=0;
+        int i=0;
+        while (list.get(i) < list.get(i+1)) {
+            lp= i+2;
+            rp= i+1;
+            i++;
+        }
+        if(PairSumFun(list, target)){
+            System.out.println("solution exist");
+        }else{
+            System.out.println("Solution doesn't exist");
+           
+        }
+    }
+}
